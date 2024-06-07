756e158a249b74ba7cb9af5c4a4ed794ec84facc
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2024-06-07 21:57:27 +0900

    test

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,7 +22,6 @@ class ProfilesController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:user_name, :last_name, :first_name, :avatar, :favorite_player,
-                                 :favorite_viewing_block)
+    params.require(:user).permit(:user_name, :last_name, :first_name, :avatar, :favorite_player, :favorite_viewing_block)
   end
 end
