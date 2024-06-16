848de5b60726e9314d8dd8107197e8edd228baff
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2024-06-16 11:48:25 +0900

    UserMatchの作成

---
+++ db/migrate/20240616024803_create_user_matches.rb
@@ -0,0 +1,10 @@
+class CreateUserMatches < ActiveRecord::Migration[7.1]
+  def change
+    create_table :user_matches do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :match, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
