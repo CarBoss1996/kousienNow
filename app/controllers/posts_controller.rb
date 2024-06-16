6f7b91ce76bfbc6a9ed57d18a181ad00270aff57
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2024-06-17 01:06:43 +0900

    座席情報のリンク

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @posts = Post.all.order(created_at: :asc)
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
