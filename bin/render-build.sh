364ca3eb1ba194721b1b4343926bde3ba25354e0
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2024-06-26 22:15:10 +0900

    assetsの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,7 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails assets:precompile
 bundle exec rake db:migrate
 bundle exec rails db:seed
