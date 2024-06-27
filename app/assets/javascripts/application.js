2b9d9ebd9fcadd64f4fc294bb4abaf1eca97791b
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2024-06-27 23:01:43 +0900

    アイコンクリックしてpostを表示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
+//= require jquery
 //= require rails-ujs
 //= require turbo
 //= require ./controllers
 //= require bootstrap
 //= require confirm
+//= require user_location
--- app/assets/javascripts/user_location.js
+++ app/assets/javascripts/user_location.js
@@ -1,23 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
   const icons = document.querySelectorAll('.seat-icon');
-  const image = document.getElementById('stadium-image');
   icons.forEach(icon => {
     icon.addEventListener('click', function(event) {
       const tooltip = this.nextElementSibling;
+
+      // アイコンの位置を取得
+      const iconTop = parseFloat(icon.style.top);
+      const iconLeft = parseFloat(icon.style.left);
+
+      // ツールチップの位置を設定
+      tooltip.style.top = (iconTop - 8) + '%';
+      tooltip.style.left = (iconLeft + 1) + '%';
+
       tooltip.style.display = 'block';
-      // 画像の現在の大きさに対するパーセンテージを計算
-      const x = event.clientX / image.offsetWidth * 100;
-      const y = event.clientY / image.offsetHeight * 100;
-      tooltip.style.left = `${x}%`;
-      tooltip.style.top = `${y}%`;
+
+      // アイコンに影を追加
+      icon.style.boxShadow = '5px 5px 5px 0px rgba(0, 0, 0, 0.8)';
+      event.stopPropagation();
     });
   });
+
   document.addEventListener('click', function(event) {
     const tooltips = document.querySelectorAll('.tooltip');
     tooltips.forEach(tooltip => {
       if (!tooltip.contains(event.target)) {
         tooltip.style.display = 'none';
       }
+    });
+
+// クリックされた場所がアイコン以外の場合、全てのアイコンの影を削除
+    icons.forEach(icon => {
+    icon.style.boxShadow = '';
     });
   });
 });
