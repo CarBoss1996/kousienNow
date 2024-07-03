b2cb501d5243270a7e7fd0ef4e5ee077c23e5302
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2024-07-03 12:04:52 +0900

    match表示修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,4 +3,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:clean
 bundle exec rails assets:precompile
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:migrate
+bundle exec rails db:seed
