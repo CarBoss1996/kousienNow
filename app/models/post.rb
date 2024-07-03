d51c59eb9dd23c0ad34e287c4dc7dd8820bd68c9
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2024-07-03 23:56:24 +0900

    ransackの検索機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :like_posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["body", "created_at", "id", "id_value", "image", "updated_at", "user_id"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[comments image_attachment image_blob like_posts user]
+  end
 end
