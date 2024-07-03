e7a8698b3af84dc516c8657afd57db2b77d3d77c
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2024-07-03 11:55:21 +0900

    match表示修正

--- app/controllers/matchs_controller.rb
+++ app/controllers/matchs_controller.rb
@@ -5,9 +5,6 @@ require 'uri'
 class MatchsController < ApplicationController
   def index
     @matches = Match.where(match_date: Date.today.beginning_of_day..Date.today.end_of_day)
-            .group(:match_date)
-            .maximum(:created_at)
-            .map { |match_date, created_at| Match.find_by(match_date: match_date, created_at: created_at) }
     @weather = fetch_weather
   end
 
--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,4 @@ set -o errexit
 bundle install
 bundle exec rails assets:clean
 bundle exec rails assets:precompile
-bundle exec rake db:migrate
-bundle exec rails db:seed
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
