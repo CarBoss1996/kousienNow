6a23f289f61388b01a475790c0b6f5857e7c1380
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2024-07-05 09:52:26 +0900

    timezoneの設定

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Myapp
 
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Tokyo'
+
     config.autoload_paths += %W[#{config.root}/app/uploaders]
 
     # Configuration for the application, engines, and railties goes here.
