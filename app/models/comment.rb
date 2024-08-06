cd4b5e3e053baffdc1d3255591aa2920ce6c97db
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2024-08-07 01:31:22 +0900

    コメント通知修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,7 +13,7 @@ class Comment < ApplicationRecord
     # line_user_idが存在する場合のみ、LINE通知を送信
     if line_user_id
       message = "#{self.user.user_name}さんがあなたの投稿にコメントしました：#{self.body}"
-      Notification.create_and_send_line_notification(self.user, self.post, message, line_user_id)
+      Notification.create_and_send_line_notification(message, line_user_id)
     end
   end
 end
