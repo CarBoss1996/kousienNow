cfe8bcb7c4207b912115d36dafe0b3cfe1074d8e
Kawashita Natsumi <aknatu.bgtn@gmail.com>
2024-08-15 12:07:13 +0900

    通知機能実装完了

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -15,7 +15,7 @@ class Comment < ApplicationRecord
 
     # line_user_idが存在する場合のみ、LINE通知を送信
     if line_user_id.present? && line_user_id.is_a?(String)
-      message = "#{self.post.user.user_name}さん、あなたの投稿に#{self.user.user_name}さんからコメントがあります。#{self.body}"
+      message = "#{self.post.user.user_name}さん、あなたの投稿に#{self.user.user_name}さんからコメントがあります。\n\"#{self.body}\""
       Notification.create_and_send_line_notification(line_user_id, message)
     end
   end
